116bd9c525c991c6ba09ef5675ed61944b48535b
arionbat <haoshuai0913@icloud.com>
2026-01-06 13:26:05 +0800

    feat: 修改build文件

--- plugin/sync/build.gradle.kts
+++ plugin/sync/build.gradle.kts
@@ -1,12 +1,18 @@
 plugins {
     id("org.fcitx.fcitx5.android.app-convention")
     id("org.fcitx.fcitx5.android.plugin-app-convention")
+    id("org.fcitx.fcitx5.android.build-metadata")
+    id("org.fcitx.fcitx5.android.data-descriptor")
     alias(libs.plugins.kotlin.serialization)
 }
 
 android {
     namespace = "org.fcitx.fcitx5.android.plugin.sync"
 
+    buildFeatures {
+        buildConfig = true
+    }
+    
     defaultConfig {
         applicationId = "org.fcitx.fcitx5.android.plugin.sync"
     }
